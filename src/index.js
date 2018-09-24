e638dcb2eb5cbf390fd1de0f0eb430ab02019a8e
Nick Bohannan <nbohannan@gmail.com>
2018-09-24 17:31:48 -0500

    first upload to heroku

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import registerServiceWorker from './registerServiceWorker';
 class Home extends Component {
     render() {
         return (
-            <div className="welcometext">my name's nick. i'm a web developer.</div>
+            <div className="welcometext">hello! my name's nick and i'm a web developer.</div>
         )
     }
 }
@@ -25,57 +25,69 @@ class Work extends Component {
     render() {
         return (
             <div>
-                <div className="work-container">
-                    <div className="work-element">
-                        <div className="title-main">Houndstooth Woodwerk</div>
-                        <img src={require('./houndstooth.png')} alt="Houndstooth" />
-                        <div className="title">Description</div>
-                        <div className="description"></div>
-                        <div className="title-ul">Libraries/Frameworks Used</div>
-                        <div className="tools">
-                            <a>Node.js</a>
-                            <a>Express</a>
-                            <a>HTML/CSS</a>
-                            <a>React</a>
-                        </div>
+                <div className="work-wrapper">
+                    <div className="work-container">
+                        <WorkEntry
+                            name="Houndstooth Woodwerk"
+                            image={require('./houndstooth.png')}
+                            description="Dynamic web presence for carpentry business"
+                            tools1="Node.js"
+                            tools2="Express"
+                            tools3="HTML/CSS"
+                            tools4="React"
+                        />
+                        <WorkEntry
+                            name="Compendium of Ghara"
+                            image={require('./ghara.png')}
+                            description="PostgreSQL database with custom UI web portal"
+                            tools1="Node.js"
+                            tools2="Express"
+                            tools3="EJS Template Engine"
+                            tools4="PostgreSQL"
+                            tools5="Sequelize"
+                            tools6="HTML/CSS/JS"
+                        />
+                        <WorkEntry
+                            name="Bohannan Medical"
+                            image={require('./bomed.png')}
+                            description="eCommerce site for orthopedic devices"
+                            tools1="Wordpress CMS"
+                            tools2="HTML/CSS"
+                        />
+                        <WorkEntry
+                            name="Personal Site"
+                            image={require('./personal.png')}
+                            description="Personal portfolio site made with React"
+                            tools1="HTML/CSS"
+                            tools2="React"
+                        />
                     </div>
-                    <div className="work-element">
-                        <div className="title-main">Compendium of Ghara</div>
-                        <img src={require('./ghara.png')} alt="Compendium" />
-                        <div className="title">Description</div>
-                        <div className="description"></div>
-                        <div className="title-ul">Libraries/Frameworks Used</div>
-                        <div className="tools">
-                            <a>Node.js</a>
-                            <a>Express</a>
-                            <a>EJS Template Engine</a>
-                            <a>PostgreSQL</a>
-                            <a>Sequelize</a>
-                            <a>HTML/CSS/JS</a>
-                        </div>
-                    </div>
-                    <div className="work-element">
-                        <div className="title-main">Bohannan Medical</div>
-                        <img src={require("./bomed.png")} alt="Bohannan Medical" />
-                        <div className="title">Description</div>
-                        <div className="description"></div>
-                        <div className="title-ul">Tools Used</div>
-                        <div className="tools">
-                            <a>Wordpress CRM</a>
-                            <a>HTML/CSS</a>
-                        </div>
-                    </div>
-                    <div className="work-element">
-                        <div className="title-main">Something</div>
-                        <img src="" alt="Dunno yet" />
-                        <div className="title">Description</div>
-                        <div className="description"></div>
-                        <div className="title">Tools Used</div>
-                        <div className="tools"></div>
+                </div >
+                <div className="github-link-container">
+                    <div className="github-link">
+                        <img src={require("./GitHub_Logo_White.png")} onClick={this.githubLink} />
                     </div>
                 </div>
-                <div className="github-link">
-                    <img src={require("./GitHub_Logo_White.png")} onClick={this.githubLink} />
+            </div>
+        )
+    }
+}
+
+class WorkEntry extends Component {
+    render() {
+        return (
+            <div className="work-element">
+                <div className="title-main">{this.props.name}</div>
+                <img src={this.props.image} alt="Dunno yet" />
+                <div className="description">{this.props.description}</div>
+                <div className="title-ul">Tools Used</div>
+                <div className="tools">
+                    <a>{this.props.tools1}</a>
+                    <a>{this.props.tools2}</a>
+                    <a>{this.props.tools3}</a>
+                    <a>{this.props.tools4}</a>
+                    <a>{this.props.tools5}</a>
+                    <a>{this.props.tools6}</a>
                 </div>
             </div>
         )
